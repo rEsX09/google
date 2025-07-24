74c09b96f53254945b45b5f69447f7643ce44c7b
rEsX09 <robchik439@gmail.com>
2025-07-24 13:20:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -14,19 +14,3 @@ def show_form():
 def handle_form():
     email = request.form['email']
     password = request.form['password']
-
-    # Saglabāšana failā:
-    @app.route('/show-credentials/')
-def show_credentials():
-    auth = request.args.get('auth')
-    if auth != 'Robertins1':
-        abort(403)  # Aizliegts piekļūt, ja parole nav pareiza
-
-    try:
-        with open("credentials.txt", "r", encoding="utf-8") as file:
-            data = file.read()
-    except FileNotFoundError:
-        data = "Nav saglabātu datu."
-
-    # Parāda datus vienkāršā lapā
-    return f"<pre>{data}</pre>"
